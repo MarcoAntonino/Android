754fb67a2b0893bf446ed827d7b65b8e8a2af827
Marco Antonino <antoninom@AULE.IT>
2017-03-27 18:02:58 +0200

    Creata app hello world

---
+++ MyTest/app/src/main/java/com/example/antoninom/mytest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.antoninom.mytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*
+        La classe r viene generata dal compilatore ed è collegata a tutte le risorse e quindi io accedo a tutte
+        le risorse
+         */
+    }
+}
